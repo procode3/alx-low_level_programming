45884788598a4d7d8f5b02b121a75b500665aed9
Erick <wr.t8r1@gmail.com>
2022-09-08 06:01:32 -0700

    The printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point.
+ * main: - Entry point.
  *
  * return: Always 0.
  */
 
-int main(void)
+int main (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
