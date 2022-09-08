adbec4ff6538731702a6705db2fbd4accc81c4fb
Erick <wr.t8r1@gmail.com>
2022-09-08 05:15:51 -0700

    Cant figure this Betty out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main: The prgram entry point
- *
- * return: Normally 0 (success)
+ * main -Entry point.
+ * 
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
