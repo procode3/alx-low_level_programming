43880441873f72db09d2a53c823b31774a782195
Erick <wr.t8r1@gmail.com>
2022-09-08 06:32:48 -0700

    Sizeof command to display size of variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long longType;
+	
+	/* sizeof evaluates the size of a variable*/
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return 0;
+}
