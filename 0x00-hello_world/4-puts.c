2a8e82cb5b51e21dd30c7d8fb4203e5f297269fd
Erick <wr.t8r1@gmail.com>
2022-09-08 05:44:43 -0700

    Final attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * Return: Always zero.
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
