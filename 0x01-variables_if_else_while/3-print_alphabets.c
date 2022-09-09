466968750fae255a88c790980e4139eb1c6004a3
Erick <wr.t8r1@gmail.com>
2022-09-09 15:48:04 -0700

    Displaying alphabets in lower and upercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,13 @@
 
 int main(void)
 {
-	for (int l_case = 'a'; l_case <= 'z'; l_case++)
+	int l_case, u_case;
+
+	for (l_case = 'a'; l_case <= 'z'; l_case++)
 	{
 		putchar(l_case);
 	}
-	for (int u_case = 'A'; u_case <= 'Z'; u_case++)
+	for (u_case = 'A'; u_case <= 'Z'; u_case++)
 	{
 		putchar(u_case);
 	}
