a69094f4c12b6fb11b387f4d7b37f89f8ebf2b24
Erick <wr.t8r1@gmail.com>
2022-09-09 18:29:25 -0700

    Displaying a combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,24 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 58; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j = 49; j <= 58; j++)
 		{
-			if (j > i)
+			for (k = 50; k <= 58; k++)
 			{
-				putchar(i);
-				putchar(j);
-				if (i != 56 || j != 57)
+				if (k > j && j > i)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{	
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
