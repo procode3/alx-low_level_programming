441254522e8335040645600e728d93c122ddf4f5
Erick <wr.t8r1@gmail.com>
2022-09-13 11:06:00 -0700

    Printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,21 @@
 
 int main(void)
 {
-	putchar("_putchar\n");
+	int i;
+	char vname[] = "_putchar";
+
+	for (i = 0; i < 80; i++)
+	{
+		if (vname[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(vname[i]);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
