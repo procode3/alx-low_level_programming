aaa65fa6cb065f3a2ba7768801b57e902144d694
Erick <wr.t8r1@gmail.com>
2022-09-13 13:07:49 -0700

    Displaying alphabets with a non-standard .h file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet - prints the letters of the alphabet
+ *
+ * Return: 0 Always
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+	return (0);
+}
