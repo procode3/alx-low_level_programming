068a5ecbe4947f0ee76b0b67e122619e049b3b5b
Erick <wr.t8r1@gmail.com>
2022-09-14 00:38:24 -0700

    Jack bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,18 +10,18 @@ void jack_bauer(void)
 {
 	int h, m;
 	
-	int h =0;
+	h = 0;
 
 	while (h <= 24)
 	{
 		m = 0;
 		while (m <= 60)
 		{
-			putchar((h / 10) + '0');
-			putchar((h % 10) + '0');
-			putchar(':');
-			putchar((m / 10) + '0');
-			putchar((m % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			m++;
 		}
 		h++;
