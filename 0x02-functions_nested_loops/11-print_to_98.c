7c2127a07ebe76589dcb6f15de253b4bebbea2d8
Erick <wr.t8r1@gmail.com>
2022-09-14 07:59:19 -0700

    Printingclear

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 
 	if (n > 99)
 	{
-		_putchar(' ');
+
 		_putchar(n / 100 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
@@ -34,6 +34,7 @@ void print_to_98(int n)
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
+		putchar('\n');
 	}
 	else if (n < 100 && n >= 98)
 	{
