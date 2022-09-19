5667ae090c0fdf8afac67c22586283ac358aa114
Erick <wr.t8r1@gmail.com>
2022-09-19 16:57:21 -0700

    Printing anyother chr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,16 +3,20 @@
 /**
  *  puts2 - prints string
  * @str: useless str
- *  
+ *
  *  Return: a string
  */
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int c;
+
+	for (c = 0; *(str + c) != 0; i++)
 	{
-		_putchar(*str);
-		str += 2;
+		while (c % 2 == 0)
+		{
+		_putchar (*(str + i));	
+		}
 	}
 	_putchar('\n');
 }
