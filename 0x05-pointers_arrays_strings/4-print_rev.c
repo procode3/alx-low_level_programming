a09bc5388e2c40b26d7ef0a5a73d5aabd21daba9
Erick <wr.t8r1@gmail.com>
2022-09-19 15:09:23 -0700

    Reverse str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: useless str
+ *
+ * Return: noting
+ */
+
+void print_rev(char *s)
+{
+	int c, len, tmp;
+
+	len = _strlen(*s);
+
+	for (c = 0; c < len / 2; c++)
+	{
+		tmp = s[c];
+		s[c] = s[len - i - 1]
+		s[len - i - 1] = tmp;
+	}
+}
