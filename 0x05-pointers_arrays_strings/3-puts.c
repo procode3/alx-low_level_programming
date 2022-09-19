82aa520effefc2295ceee21c6dfd38a09afc5fd8
Erick <wr.t8r1@gmail.com>
2022-09-19 14:46:59 -0700

    Printing str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _puts - prints string
+ * @str: useless str
+ *  Return: a string
+ */
+
+void _puts(char *str)
+{
+	while (*ch = getchar(), *ch >= 0)
+	{
+		putchar(*ch);
+	}
+}
