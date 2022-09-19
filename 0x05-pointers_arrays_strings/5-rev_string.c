a1cd7a5b05733f407fb3854b23463df836e685dd
Erick <wr.t8r1@gmail.com>
2022-09-19 16:45:24 -0700

    Reverse str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,26 +10,18 @@
 void rev_string(char *s)
 {
 	int len, i;
-	char *lst, *fst, temp;
+	char temp;
 
 	for (len = 0; s[len] != 0; len++)
 	{
 
 	}
-	fst = s;
-	lst = s;
 
 
 	for (i = 0; i < (len - 1); i++)
 	{
-		for (i = 0; i < len / 2; i++)
-		{
-			temp = *lst;
-			*lst = *fst;
-			*fst = temp;
-
-			fst++;
-			lst--;
-		}
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
