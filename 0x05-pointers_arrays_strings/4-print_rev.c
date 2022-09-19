f06bc4c42e152ea3509719e8640740845719d6dc
Erick <wr.t8r1@gmail.com>
2022-09-19 15:12:27 -0700

    Reverse str

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@ void print_rev(char *s)
 {
 	int c, len, tmp;
 
-	len = _strlen(*s);
+	len = _strlen(s);
 
 	for (c = 0; c < len / 2; c++)
 	{
 		tmp = s[c];
-		s[c] = s[len - i - 1]
+		s[c] = s[len - c - 1]
 		s[len - i - 1] = tmp;
 	}
 }
