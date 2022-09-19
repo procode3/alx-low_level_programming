a914e55fd07e5dccbaace8d30e6ca641978cc11a
Erick <wr.t8r1@gmail.com>
2022-09-19 09:33:21 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two numbers
+ * @a: useless int
+ * @b: useless int
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *temp;
+	
+	temp = *(&b);
+	*(&b) = *(&a);
+	*(&a) = temp;
+}
