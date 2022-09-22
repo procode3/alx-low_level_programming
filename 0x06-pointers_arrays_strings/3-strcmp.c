0ef83f4273717ec4936624b3a576af2d96d1beb9
Erick <wr.t8r1@gmail.com>
2022-09-22 10:57:13 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,11 +15,13 @@ int _strcmp(char *s1, char *s2)
 	int flag = 0, i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-		if (s1[i]! = s2[i])
-			flag = 1;
+		if (s1[i] != s2[i])
+		{
+			flag = -15;
 			break;
+		}
 	if (flag == 0)
 		return (0);
 	else
-		return (1);
+		return (15);
 }
