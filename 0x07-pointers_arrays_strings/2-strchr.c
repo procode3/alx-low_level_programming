3ab925c492cb1c3ff8a1f53c760eabf170a38ba5
Erick <wr.t8r1@gmail.com>
2022-09-26 14:18:01 -0700

    Return char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h> 
+
+/**
+ * _strchr - returns a chr in strings
+ * @s: useless string
+ * @c: useless char
+ *
+ * Return: pointer or Null
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+		return (s);
+		}
+
+	}
+	while (*s++);
+		{
+		return (NULL);
+		}
+
+}
