cab346d92fe3d94dc66b5750dd8780224038532d
Erick <wr.t8r1@gmail.com>
2022-09-28 14:58:33 -0700

    Revrecursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse str
+ * @s: str
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
