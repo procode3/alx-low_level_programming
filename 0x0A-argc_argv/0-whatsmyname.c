1483bc656f49770693de8194d7317bb9a6c07346
Erick <wr.t8r1@gmail.com>
2022-09-30 17:54:31 -0700

    D

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,10 +10,10 @@
  * Return: Always 0 (Success)
 */
 
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	(void) argc;
 
-	printf("%s\n", argv[i]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
