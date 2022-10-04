f5e05a64d9155a3127743312ab5ebb769fc2bb98
Erick <wr.t8r1@gmail.com>
2022-10-04 14:07:56 -0700

    Concatinate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat  - concantinates
+ * @s1: str
+ * @s2: str
+ *
+ * Return: Concatinated char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int total, i, j = 0;
+
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (i = 0; s1[i] || s2[i]; i++)
+
+	ptr = (char *)malloc(sizeof(char) * i);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		ptr[j++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		ptr[j++] = s2[i];
+
+	return (ptr);
+}
