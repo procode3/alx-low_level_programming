7d55b9005aa20af597130d77556a688d9b70260b
Erick <wr.t8r1@gmail.com>
2022-10-06 13:07:00 -0700

    Concat to n

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,15 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 
-	unsigned int s1_len = 0, s2_len = 0, i, j = 0;
+	unsigned int s1_len = 0, s2_len = 0, i = 0, j = 0;
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 
 	if (n < s2_len)
-		ptr = (char *)(malloc(sizeof(char) * (s1_len + n + 1)));
+		ptr = malloc(sizeof(char) * (s1_len + n + 1));
 	else
-		ptr = (char *)(malloc(sizeof(char) * (s1_len + s2_len + 1)));
+		ptr = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
 	if (!ptr)
 		return (NULL);
@@ -34,14 +34,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 
-	for (; n < s2_len && i < (s1_len + n); i++)
+	while (n < s2_len && i < (s1_len + n))
 	{
-		ptr[i] = s2[j++];
+		ptr[i++] = s2[j++];
 	}
 
-	for (; n >= s2_len && i < (s1_len + s2_len); i++)
+	while (n >= s2_len && i < (s1_len + s2_len))
 	{
-		ptr[i] = s2[j++];
+		ptr[i++] = s2[j++];
 	}
 
 	ptr[i] = '\0';
