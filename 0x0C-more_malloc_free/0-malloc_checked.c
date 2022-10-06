44280b80aca48864ce42f6a9420350739f6ec355
Erick <wr.t8r1@gmail.com>
2022-10-06 11:33:51 -0700

    Malloc(b)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <strlib.h>
+
+/**
+ * *malloc_checked - allocates memory to an int
+ * @b: useless int
+ *
+ * Return: 98 for an error
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
