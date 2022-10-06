2b61fabd99c6322d84f69b288413dc7a3ad6cb3f
Erick <wr.t8r1@gmail.com>
2022-10-06 13:21:40 -0700

    Concant to n

--- 0x0C-more_malloc_free/1-string_nconcatb.c
+++ 0x0C-more_malloc_free/1-string_nconcatb.c
@@ -15,35 +15,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 
-	unsigned int s1_len = 0, s2_len = 0, i = 0, j = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-
-	if (n < s2_len)
-		ptr = malloc(sizeof(char) * (s1_len + n + 1));
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		ptr = malloc(sizeof(char) * (s1_len + s2_len + 1));
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!ptr)
 		return (NULL);
+	while (i < len1)
 
-	for (i = 0; i < s1_len; i++)
 	{
 		ptr[i] = s1[i];
+		i++;
 	}
-
-
-	while (n < s2_len && i < (s1_len + n))
-	{
+	while (n < len2 && i < (len1 + n))
 		ptr[i++] = s2[j++];
-	}
-
-	while (n >= s2_len && i < (s1_len + s2_len))
-	{
+	while (n >= len2 && i < (len1 + len2))
 		ptr[i++] = s2[j++];
-	}
 
 	ptr[i] = '\0';
+
 	return (ptr);
 }
