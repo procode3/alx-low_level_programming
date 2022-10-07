b6bb0cdf5ad8f791ab06aaedf24a9cd23fedae49
Erick <wr.t8r1@gmail.com>
2022-10-07 02:12:21 -0700

    Updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,8 +28,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr = malloc(sizeof(char) * (s1_len + n + 1));
 	else
 		ptr = malloc(sizeof(char) * (s1_len + s2_len + 1));
-	
-	
+
+
 
 	if (!ptr)
 		return (NULL);
