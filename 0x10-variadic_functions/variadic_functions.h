f02c4890126cdb02906edb8862f2b56d67dbbf1d
Erick <wr.t8r1@gmail.com>
2022-10-14 16:59:47 -0700

    Msg

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,27 @@
 
 int sum_them_all(const unsigned int n, ...);
 
-#endif /*Variadic Functions*/
+
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+
+/**
+ * struct print - A new struct
+ * @symbol: data type
+ * @print_func: a function pointer that prints
+ */
+
+typedef struct print
+{
+	char *symbol;
+	void (*print_func)(va_list arg);
+} func_printer;
+
+#endif /*Variadic Functions*/
