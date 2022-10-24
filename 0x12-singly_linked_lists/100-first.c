dd647161cd715b48acf7a77e62095b26bdb622c8
Erick <wr.t8r1@gmail.com>
2022-10-24 15:16:08 -0700

    A program that runs before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * premain - runs before main
+ *
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
