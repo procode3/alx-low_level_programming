1d89b86f77ed2d327ac051008cafc092f47ab806
Erick <wr.t8r1@gmail.com>
2022-10-24 14:18:39 -0700

    Adding a function that adds a new node at head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the begining of a list
+ * @head: pointer to pointers of first elem in list_t list
+ * @str: pointer to new elem to be added
+ *
+ * Return: new list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	while(head)
+	{
+		stt -> next = *head;
+	}
+}
