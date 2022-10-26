c6dbb8b0f7251e85a1230146044280a94e7aefc6
Erick <wr.t8r1@gmail.com>
2022-10-26 14:30:08 -0700

    Frees a linkedlist

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees memory of a linkedlist
+ * @head: pointer to 1st elem of a linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		*head = temp;
+		temp = temp->next;
+		free(*head);
+	}
+	*head = NULL;
+}
