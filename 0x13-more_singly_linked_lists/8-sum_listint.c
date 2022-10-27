dccc0ad47e311298851c3c066fdbd1513bb870f1
Erick <wr.t8r1@gmail.com>
2022-10-27 02:24:11 -0700

    Add a functions that sums all data in a linkedlist

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - functions that sums all data in a listint_t list
+ * @head: pointer to 1st node of listint_t list
+ *
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	size_t sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (head->next != NULL)
+	{
+		head = head->next;
+		sum += head->n;
+	}
+	
+
+	return (sum);
+}
