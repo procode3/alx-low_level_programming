99b149575b5543a7529c457106cffbf20645e12e
Erick <wr.t8r1@gmail.com>
2022-11-02 16:15:06 -0700

    updated null string to empty strin

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return (-1);
+		text_content= "";
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
