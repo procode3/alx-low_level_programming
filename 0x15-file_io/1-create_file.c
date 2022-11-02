b6c8c738808d8915014a7dc6d45a3498d83edc91
Erick <wr.t8r1@gmail.com>
2022-11-02 16:07:49 -0700

    Updated if condition for checking NULLness of text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,11 @@ int create_file(const char *filename, char *text_content)
 	int fd, file;
 	size_t len;
 
-	char *size;
-
-	size = malloc(sizeof(text_content));
-	if (!size)
-		return (-1);
 
 	if (!filename)
+		return (-1);
+
+	if (!text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -32,5 +30,6 @@ int create_file(const char *filename, char *text_content)
 
 	file = write(fd, text_content, len);
 
+	close(fd);
 	return ((file < 0) ? (-1) : 1);
 }
