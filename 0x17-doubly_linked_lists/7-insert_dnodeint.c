62270d6515f27937d40422daec690d3172e5d52f
Erick <wr.t8r1@gmail.com>
2022-12-08 14:39:03 -0800

    Fixed a terrible error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,10 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new = malloc(sizeof(dlistint_t));
-	if(new == NULL)
+	if (new == NULL)
 	{
-		return NULL;
-	}	
+		return (NULL);
+	}
 	new->prev = NULL;
 	new->n = n;
 	new->next = NULL;
