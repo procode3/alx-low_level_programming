5f844eb70f4621480951542a2a0b4ad529c456cf
Erick <wr.t8r1@gmail.com>
2022-12-08 14:04:59 -0800

    Fixed for inserting at index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	temp = *h;
-	if (temp->prev == NULL & idx == 0)
+	if (temp->prev == NULL && idx == 0)
 	{
 		if (temp->next == NULL)
 		{
