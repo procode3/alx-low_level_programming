bba7ccfcba8df84d1d9073a8e961ef5c17753ce1
procode3 <wr.t8r1@gmail.com>
2023-04-13 22:59:17 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,26 +10,26 @@ int bs(int *arr, size_t low, size_t high, int value);
  */
 
 int binary_search(int *array, size_t size, int value)
-{	
+{
 	int idx;
-    size_t mid = (size - 1) / 2;
+	size_t mid = (size - 1) / 2;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    print_array(array, size);
+	print_array(array, size);
 
-    if (array[mid] == value)
-        return (mid);
+	if (array[mid] == value)
+		return (mid);
 
-    if (array[mid] > value)
-        return (binary_search(array, mid, value));
+	if (array[mid] > value)
+		return (binary_search(array, mid, value));
 
-    idx = binary_search(array + mid + 1, size - mid - 1, value);
-    if (idx != -1)
-        return (idx + mid + 1);
-    else
-        return (-1);
+	idx = binary_search(array + mid + 1, size - mid - 1, value);
+	if (idx != -1)
+		return (idx + mid + 1);
+	else
+		return (-1);
 }
 
 /**
@@ -44,13 +44,13 @@ void print_array(int *arr, size_t size)
 	size_t i;
 
 	printf("Searching in array: ");
-	for(i = 0; i < size; i++)
-        {
-			printf("%d", arr[i]);
-            if(i != size - 1)
-            {
-                printf(", ");
-            }
-        }
-	printf("\n"); 
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", arr[i]);
+		if (i != size - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
