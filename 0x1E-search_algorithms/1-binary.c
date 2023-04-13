1e9849bbf1199f4d69e0a37eb9965356878605a5
procode3 <wr.t8r1@gmail.com>
2023-04-13 22:57:34 +0300

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+void print_array(int *arr, size_t size);
+int bs(int *arr, size_t low, size_t high, int value);
+/**
+ * binary_search - perform a binary search in an array
+ * @array: an int array
+ * @size: size of array
+ * @value: value to be searched
+ * Return: index of value or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{	
+	int idx;
+    size_t mid = (size - 1) / 2;
+
+    if (array == NULL || size == 0)
+        return (-1);
+
+    print_array(array, size);
+
+    if (array[mid] == value)
+        return (mid);
+
+    if (array[mid] > value)
+        return (binary_search(array, mid, value));
+
+    idx = binary_search(array + mid + 1, size - mid - 1, value);
+    if (idx != -1)
+        return (idx + mid + 1);
+    else
+        return (-1);
+}
+
+/**
+ * print_array - prints an array
+ * @arr: pointer to start of array
+ * @size: size of array
+ * Return: nothing
+ */
+
+void print_array(int *arr, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for(i = 0; i < size; i++)
+        {
+			printf("%d", arr[i]);
+            if(i != size - 1)
+            {
+                printf(", ");
+            }
+        }
+	printf("\n"); 
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,7 @@
+C search algorithms:
+	i) Linear search
+	ii) Binary Search
+	iii) Jump Search
+	iv) Interpolation Search
+	v) Exponential Search
+Author: Erick Barasa
